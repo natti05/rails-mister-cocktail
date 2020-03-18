c6a388ac6a42e84f269f737d9b600a6105594819
Natalia Takahara <nana_takahara@hotmail.com>
2020-03-18 19:32:39 +0900

    add grid and form in show page

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,10 +5,12 @@ class CocktailsController < ApplicationController
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
   end
 
   def new
     @cocktail = Cocktail.new
+
   end
 
   def create
